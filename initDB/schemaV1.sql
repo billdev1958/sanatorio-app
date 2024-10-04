5c354a30d2ed3094d6fa29fa55f962b086bb3071
billdev1958 <billdev1958@gmail.com>
2024-10-04 16:14:00 -0600

    se agrega dependencias y afiliacion al schema

--- initDB/schemaV1.sql
+++ initDB/schemaV1.sql
@@ -1,3 +1,43 @@
+-- ====================================
+-- Tablas principales
+-- ====================================
+
+-- Tabla de dependencias
+CREATE TABLE IF NOT EXISTS cat_dependencies(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(70),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS affiliation_patient (
+    id SERIAL PRIMARY KEY,
+    dependency_id INTEGER,
+    name VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de cuentas de usuario
+CREATE TABLE IF NOT EXISTS account (
+    id UUID PRIMARY KEY,
+    affiliation_id INTEGER NOT NULL,
+    telefono VARCHAR(10) NOT NULL,
+    email VARCHAR(75) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    password_change_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- ====================================
+-- Tablas de roles y permisos
+-- ====================================
+
 -- Tabla de roles
 CREATE TABLE IF NOT EXISTS role (
     id SERIAL PRIMARY KEY,
@@ -17,7 +57,7 @@ CREATE TABLE IF NOT EXISTS permissions (
 );
 
 -- Tabla intermedia para rol y permiso (relación muchos a muchos)
-CREATE TABLE IF NOT EXISTS role_permission(
+CREATE TABLE IF NOT EXISTS role_permission (
     id_role INTEGER NOT NULL,
     id_permission INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -27,40 +67,21 @@ CREATE TABLE IF NOT EXISTS role_permission(
 );
 
 -- Tabla para asociar roles a usuarios
-CREATE TABLE IF NOT EXISTS user_roles(
-    id SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS user_roles (
+    id SERIAL PRIMARY KEY,
     account_id UUID NOT NULL, -- Relación con la cuenta/usuario
-    role_id INTEGER NOT NULL,    -- Relación con roles
+    role_id INTEGER NOT NULL, -- Relación con roles
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     UNIQUE (account_id, role_id) -- Clave única para evitar duplicados
 );
 
-CREATE TABLE IF NOT EXISTS account (
-    id UUID PRIMARY KEY,
-    telefono VARCHAR(10) NOT NULL,
-    email VARCHAR(75) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    password_change_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
+-- ====================================
+-- Tablas relacionadas con pacientes, médicos y beneficiarios
+-- ====================================
 
-CREATE TABLE IF NOT EXISTS super_user (
-    id UUID PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name1 VARCHAR(50) NOT NULL,
-    last_name2 VARCHAR(50) NOT NULL,
-    account_id UUID NOT NULL,
-    curp CHAR(18) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
+-- Tabla de pacientes
 CREATE TABLE IF NOT EXISTS patient (
     id UUID PRIMARY KEY,
     medical_history_id VARCHAR(12) NOT NULL, -- id de la tabla medical_history
@@ -76,7 +97,21 @@ CREATE TABLE IF NOT EXISTS patient (
     deleted_at TIMESTAMP
 );
 
---beneficiarios
+-- Tabla de médicos
+CREATE TABLE IF NOT EXISTS doctor (
+    id UUID PRIMARY KEY,
+    account_id UUID NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name1 VARCHAR(50) NOT NULL,
+    last_name2 VARCHAR(50) NOT NULL,
+    specialty_id INT NOT NULL,
+    medical_license VARCHAR(25) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de beneficiarios
 CREATE TABLE IF NOT EXISTS beneficiary (
     id UUID PRIMARY KEY,
     account_holder UUID NOT NULL, -- id de la cuenta principal
@@ -90,19 +125,12 @@ CREATE TABLE IF NOT EXISTS beneficiary (
     deleted_at TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS medical_history_relation (
-    id UUID PRIMARY KEY,
-    medical_history_id VARCHAR(12) NOT NULL,
-    patient_id UUID,
-    beneficiary_id UUID,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    CHECK (patient_id IS NOT NULL OR beneficiary_id IS NOT NULL)
-);
+-- ====================================
+-- Tablas relacionadas con historiales médicos
+-- ====================================
 
-
+-- Tabla de historiales médicos
 CREATE TABLE IF NOT EXISTS medical_history (
-    -- Para un futuras probabilidades de escalabilidad el identificador de expediente cambiara a UUID por ahora sera el personalizado
     id VARCHAR(12) PRIMARY KEY NOT NULL,
     date_of_record DATE NOT NULL, -- 'Fecha'
     time_of_record TIME NOT NULL, -- 'Hora'
@@ -153,6 +181,119 @@ CREATE TABLE IF NOT EXISTS medical_history (
     specialty_license VARCHAR(10) NOT NULL -- 'Cédula especialidad'
 );
 
+-- Tabla de relaciones de historial médico
+CREATE TABLE IF NOT EXISTS medical_history_relation (
+    id UUID PRIMARY KEY,
+    medical_history_id VARCHAR(12) NOT NULL,
+    patient_id UUID,
+    beneficiary_id UUID,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    CHECK (patient_id IS NOT NULL OR beneficiary_id IS NOT NULL)
+);
+
+-- ====================================
+-- Tablas relacionadas con citas y consultas
+-- ====================================
+
+-- Tabla de citas
+CREATE TABLE IF NOT EXISTS appointment (
+    id UUID PRIMARY KEY,
+    doctor_id UUID NOT NULL,
+    patient_account_id UUID NOT NULL,
+    office_id INTEGER NOT NULL,
+    time_start TIMESTAMP NOT NULL,
+    time_end TIMESTAMP NOT NULL,
+    schedule_id INTEGER NOT NULL,
+    status_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    CONSTRAINT chk_time_validity CHECK (time_start < time_end)
+);
+
+-- Tabla de consultas
+CREATE TABLE IF NOT EXISTS consultation (
+    id SERIAL PRIMARY KEY,
+    patient_id UUID NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    reason TEXT NOT NULL,
+    symptoms TEXT NOT NULL,
+    doctor_notes TEXT,
+    requested_tests TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ====================================
+-- Tablas relacionadas con oficinas, horarios y servicios
+-- ====================================
+
+-- Tabla de especialidades
+CREATE TABLE IF NOT EXISTS specialty (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(75) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de oficinas
+CREATE TABLE IF NOT EXISTS office (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    specialty_id INTEGER NOT NULL,
+    status_id INTEGER NOT NULL,
+    doctor_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de horarios
+CREATE TABLE IF NOT EXISTS schedule (
+    id SERIAL PRIMARY KEY,
+    office_id INTEGER NOT NULL,
+    day_of_week INT NOT NULL,
+    time_start TIME NOT NULL,
+    time_end TIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- ====================================
+-- Otras tablas auxiliares
+-- ====================================
+
+-- Tabla de estatus de citas
+CREATE TABLE IF NOT EXISTS appointment_status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de estatus de oficinas
+CREATE TABLE IF NOT EXISTS office_status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de servicios
+CREATE TABLE IF NOT EXISTS services (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Tabla de notas de evolución
 CREATE TABLE IF NOT EXISTS evolution_note (
     folio SERIAL PRIMARY KEY,
     fecha DATE NOT NULL,
@@ -172,6 +313,7 @@ CREATE TABLE IF NOT EXISTS evolution_note (
     notas VARCHAR(6) NOT NULL
 );
 
+-- Tabla de incapacidades
 CREATE TABLE IF NOT EXISTS incapacity (
     folio SERIAL PRIMARY KEY,
     fecha DATE NOT NULL,
@@ -187,123 +329,47 @@ CREATE TABLE IF NOT EXISTS incapacity (
     clave VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS services (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
+-- ====================================
+-- Claves foráneas
+-- ====================================
 
-CREATE TABLE IF NOT EXISTS appointment_status (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS office_status (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS office (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    specialty_id INTEGER NOT NULL,
-    status_id INTEGER NOT NULL,
-    doctor_id UUID NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS specialty (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(75) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS doctor (
-    id UUID PRIMARY KEY,
-    account_id UUID NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name1 VARCHAR(50) NOT NULL,
-    last_name2 VARCHAR(50) NOT NULL,
-    specialty_id INT NOT NULL,
-    medical_license VARCHAR(25) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS schedule (
-    id SERIAL PRIMARY KEY,
-    office_id INTEGER NOT NULL,
-    day_of_week INT NOT NULL,
-    time_start TIME NOT NULL,
-    time_end TIME NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS appointment (
-    id UUID PRIMARY KEY,
-    doctor_id UUID NOT NULL,
-    patient_account_id UUID NOT NULL,
-    office_id INTEGER NOT NULL,
-    time_start TIMESTAMP NOT NULL,
-    time_end TIMESTAMP NOT NULL,
-    schedule_id INTEGER NOT NULL,
-    status_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP,
-    CONSTRAINT chk_time_validity CHECK (time_start < time_end)
-);
-
-CREATE TABLE IF NOT EXISTS consultation (
-    id SERIAL PRIMARY KEY,
-    patient_id UUID NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    reason TEXT NOT NULL,
-    symptoms TEXT NOT NULL,
-    doctor_notes TEXT,
-    requested_tests TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS attachment (
-    id SERIAL PRIMARY KEY,
-    consultation_id INT REFERENCES consultation(id) ON DELETE CASCADE,
-    patient_id UUID REFERENCES patient(id) ON DELETE CASCADE,
-    file_path VARCHAR NOT NULL,
-    file_name VARCHAR NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Foreign keys --
-
+-- Foreign keys para la tabla account
 ALTER TABLE account
 ADD CONSTRAINT fk_role_account
 FOREIGN KEY (role_id) REFERENCES role(id);
 
+ALTER TABLE account 
+ADD CONSTRAINT fk_dependency_id
+FOREIGN KEY (affiliation_id) REFERENCES affiliation_patient(id);
+
+ALTER TABLE affiliation_patient
+ADD CONSTRAINT fk_affiliation_dependency_id
+FOREIGN KEY (dependency_id) REFERENCES cat_dependencies(id);
+
+-- Foreign keys para la tabla super_user
 ALTER TABLE super_user
 ADD CONSTRAINT fk_account_super_user
 FOREIGN KEY (account_id) REFERENCES account(id);
 
+-- Foreign keys para la tabla patient
 ALTER TABLE patient
 ADD CONSTRAINT fk_account_patient
 FOREIGN KEY (account_id) REFERENCES account(id);
 
+ALTER TABLE patient
+ADD CONSTRAINT fk_record_patient
+FOREIGN KEY (medical_history_id) REFERENCES medical_history(id);
+
+-- Foreign keys para la tabla doctor
+ALTER TABLE doctor
+ADD CONSTRAINT fk_account_doctor
+FOREIGN KEY (account_id) REFERENCES account(id);
+
+ALTER TABLE doctor
+ADD CONSTRAINT fk_specialty_doctor
+FOREIGN KEY (specialty_id) REFERENCES specialty(id);
+
+-- Foreign keys para la tabla office
 ALTER TABLE office
 ADD CONSTRAINT fk_specialty_office
 FOREIGN KEY (specialty_id) REFERENCES specialty(id);
@@ -316,18 +382,12 @@ ALTER TABLE office
 ADD CONSTRAINT fk_doctor_office
 FOREIGN KEY (doctor_id) REFERENCES doctor(id);
 
-ALTER TABLE doctor
-ADD CONSTRAINT fk_account_doctor
-FOREIGN KEY (account_id) REFERENCES account(id);
-
-ALTER TABLE doctor
-ADD CONSTRAINT fk_specialty_doctor
-FOREIGN KEY (specialty_id) REFERENCES specialty(id);
-
+-- Foreign keys para la tabla schedule
 ALTER TABLE schedule
 ADD CONSTRAINT fk_office_schedule
 FOREIGN KEY (office_id) REFERENCES office(id);
 
+-- Foreign keys para la tabla appointment
 ALTER TABLE appointment
 ADD CONSTRAINT fk_schedule_appointment
 FOREIGN KEY (schedule_id) REFERENCES schedule(id);
@@ -348,42 +408,16 @@ ALTER TABLE appointment
 ADD CONSTRAINT fk_status_appointment
 FOREIGN KEY (status_id) REFERENCES appointment_status(id);
 
+-- Foreign keys para la tabla beneficiary
 ALTER TABLE beneficiary
 ADD CONSTRAINT fk_account_holder_beneficiary
 FOREIGN KEY (account_holder) REFERENCES account(id);
-
-ALTER TABLE consultation
-ADD CONSTRAINT fk_patient_consultation
-FOREIGN KEY (patient_id) REFERENCES patient(id);
-
-
--- Roles foreing keys 
-ALTER TABLE role_permission
-    ADD CONSTRAINT fk_role_permission_role
-    FOREIGN KEY (id_role) REFERENCES role(id);
-
-ALTER TABLE role_permission
-    ADD CONSTRAINT fk_role_permission_permission
-    FOREIGN KEY (id_permission) REFERENCES permissions(id);
-
-ALTER TABLE user_roles
-    ADD CONSTRAINT fk_user_roles_account
-    FOREIGN KEY (account_id) REFERENCES account(id);
-
-ALTER TABLE user_roles
-    ADD CONSTRAINT fk_user_roles_role
-    FOREIGN KEY (role_id) REFERENCES role(id);
-
-ALTER TABLE patient
-ADD CONSTRAINT fk_record_patient
-FOREIGN KEY (medical_history_id) REFERENCES medical_history(id);
 
 ALTER TABLE beneficiary
 ADD CONSTRAINT fk_record_beneficiary
 FOREIGN KEY (medical_history_id) REFERENCES medical_history(id);
 
--- Foreign keys for medical_history_relation
-
+-- Foreign keys para la tabla medical_history_relation
 ALTER TABLE medical_history_relation
 ADD CONSTRAINT fk_medical_history_relation_medical_history
 FOREIGN KEY (medical_history_id) REFERENCES medical_history(id);
@@ -396,10 +430,34 @@ ALTER TABLE medical_history_relation
 ADD CONSTRAINT fk_medical_history_relation_beneficiary
 FOREIGN KEY (beneficiary_id) REFERENCES beneficiary(id);
 
+-- Foreign keys para la tabla role_permission
+ALTER TABLE role_permission
+ADD CONSTRAINT fk_role_permission_role
+FOREIGN KEY (id_role) REFERENCES role(id);
 
+ALTER TABLE role_permission
+ADD CONSTRAINT fk_role_permission_permission
+FOREIGN KEY (id_permission) REFERENCES permissions(id);
 
--- Validation function --
+-- Foreign keys para la tabla user_roles
+ALTER TABLE user_roles
+ADD CONSTRAINT fk_user_roles_account
+FOREIGN KEY (account_id) REFERENCES account(id);
 
+ALTER TABLE user_roles
+ADD CONSTRAINT fk_user_roles_role
+FOREIGN KEY (role_id) REFERENCES role(id);
+
+-- Foreign keys para la tabla consultation
+ALTER TABLE consultation
+ADD CONSTRAINT fk_patient_consultation
+FOREIGN KEY (patient_id) REFERENCES patient(id);
+
+-- ====================================
+-- Funciones y triggers
+-- ====================================
+
+-- Función para validar el estado de la oficina antes de programar un horario
 CREATE OR REPLACE FUNCTION validate_office_status()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -410,8 +468,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Trigger to validate office status before inserting or updating schedule --
-
+-- Trigger para validar el estado de la oficina antes de insertar o actualizar un horario
 CREATE TRIGGER trigger_validate_office_status
 BEFORE INSERT OR UPDATE ON schedule
 FOR EACH ROW EXECUTE FUNCTION validate_office_status();
