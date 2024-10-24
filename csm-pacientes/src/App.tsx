93691bf59c37576d716bd9596d5bb8f4ff69892a
billdev1958 <billdev1958@gmail.com>
2024-10-24 03:22:07 -0600

    refact

--- csm-pacientes/src/App.tsx
+++ csm-pacientes/src/App.tsx
@@ -2,8 +2,8 @@ import { createSignal, Show } from 'solid-js';
 import NavBar from './components/NavBar';
 import LateralMenu from './components/LateralMenu';
 import { JSX } from 'solid-js';
-import { useAuth } from './services/AuthContext'; // Importamos el hook de autenticación
-import { useLocation } from '@solidjs/router'; // Usamos para obtener la ruta actual
+import { useAuth } from './services/AuthContext'; 
+import { useLocation } from '@solidjs/router'; 
 
 const App = (props: { children?: JSX.Element }) => {
   const [menuOpen, setMenuOpen] = createSignal(false);
@@ -19,16 +19,13 @@ const App = (props: { children?: JSX.Element }) => {
 
   return (
     <div class="app-container">
-      {/* Siempre mostramos el NavBar */}
       <NavBar toggleMenu={toggleMenu} />
 
       <div class="main-layout">
-        {/* Mostramos el LateralMenu solo si el usuario está autenticado y no está en una ruta pública */}
         <Show when={auth.isAuthenticated() && !isPublicRoute}>
           <LateralMenu open={menuOpen()} toggleMenu={toggleMenu} />
         </Show>
 
-        {/* Ajustamos la clase content-area dependiendo del estado del menú y la autenticación */}
         <div class={`content-area ${auth.isAuthenticated() && menuOpen() && !isPublicRoute ? 'menu-open' : 'menu-closed'}`}>
           {props.children}
         </div>
