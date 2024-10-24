ded55c13d1b051a92f961baad3123222465aaf66
billdev1958 <billdev1958@gmail.com>
2024-10-23 18:39:27 -0600

    se agrega a docker el frontend

--- csm-pacientes/vite.config.ts
+++ csm-pacientes/vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
-})
+  build: {
+    outDir: 'dist',  // Asegúrate de que el directorio de salida sea 'dist'
+    target: 'esnext',  // Opcional: especifica el objetivo de ES para el bundle
+  },
+});
