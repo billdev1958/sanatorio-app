53226e6e65179360dab3f42a01700d74e47aab37
billdev1958 <billdev1958@gmail.com>
2024-11-13 20:57:41 -0600

    correccion de ruta de api

--- csm-pacientes/src/services/Api.tsx
+++ csm-pacientes/src/services/Api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://api.ax01.dev',
+    baseURL: 'https://api.ax01.dev/v1',
     headers: {
       'Content-Type': 'application/json',
     },
