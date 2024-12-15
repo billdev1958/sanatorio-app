ff3ffcb9e8de55dabdfc67ef03d1c5b857a64788
billdev1958 <billdev1958@gmail.com>
2024-12-14 22:35:07 -0600

    correccion de pagina admin

--- csm-pacientes/src/Admin/Services/Api.tsx
+++ csm-pacientes/src/Admin/Services/Api.tsx
@@ -1,11 +1,23 @@
-import axios from 'axios';
+// Api.ts
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api.ax01.dev/v1',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    timeout: 10000, // Tiempo máximo en milisegundos (10 segundos)
-  });
+  baseURL: 'https://api.ax01.dev/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000,
+});
 
-  export default api;
+// Interceptor para agregar el token si existe
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken"); 
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
