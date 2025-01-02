cf01e9d61c2dc53686f9d9eeea50bf2f5cd23057
billdev1958 <billdev1958@gmail.com>
2025-01-01 23:11:42 -0600

    fix service appointment

--- csm-pacientes/src/Citas/Models/Catalogs.tsx
+++ csm-pacientes/src/Citas/Models/Catalogs.tsx
@@ -7,3 +7,18 @@ export interface Shift{
     id: number 
     name: string
 }
+
+export interface SchedulesAppointmentRequest {
+    shift: number; // ID del turno (matutino, vespertino, etc.)
+    service: number; // ID del servicio (Medicina General, etc.)
+    appointmentDate: string; // Fecha completa en formato YYYY-MM-DD
+}
+
+export interface OfficeScheduleResponse {
+    id: number;
+    timeStart: string;
+    timeEnd: string;
+    timeDuration: string;
+    officeName: string;
+    statusID: number;
+  }
